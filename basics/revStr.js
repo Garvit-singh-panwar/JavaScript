cc17c00137bbe4570ca8cb7e278ae51ef10b738c
GarvitSingh <garvits893@gmail.com>
2025-05-28 20:23:40 +0530

    fun to rev a string

---
+++ basics/revStr.js
@@ -0,0 +1,10 @@
+// Write a program to reverse the string "javascript" using a for loop.
+function revstr(str){
+
+    for(let i = str.length - 1 ; i >= 0 ; i--){
+        console.log(str[i]);
+    }
+
+}
+
+revstr("javascript");
